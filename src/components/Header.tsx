921c11f6710a65de8cfb427790ed184ab907b753
Prathamesh Karambelkar <pkarambelkar.2002@gmail.com>
2024-06-22 16:56:59 +0530

    hero section done with animations, installed framer motion

---
+++ src/components/Header.tsx
@@ -0,0 +1,98 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+function Header(): JSX.Element {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <header className="sticky inset-x-0 top-0 z-20">
+      {/* For Large Screen */}
+      <div className="flex flex-row justify-around  border-b-2   border-slate-300 items-center w-screen  bg-baige shadow-black">
+        <Link href={"/"} className="hidden md:flex">
+          <h2 className="py-6 mx-2 ">DB</h2>
+        </Link>
+        <div className="hidden md:flex flex-col text-lg items-center md:flex-row gap-8">
+          <h5>Home</h5>
+          <h5>Lifecycle</h5>
+          <h5>Features</h5>
+          <h5>FAQ</h5>
+          <h5 onClick={() => window.open("tel:9322418319")}>Contact Us</h5>
+          <Select>
+            <SelectTrigger className="w-[180px] border-none outline-none ">
+              <SelectValue placeholder="Select a language" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup className=" border-none outline-none">
+                <SelectLabel>Language</SelectLabel>
+                <SelectItem value="en">English</SelectItem>
+                <SelectItem value="hi">Hindi</SelectItem>
+                <SelectItem value="mr">Marathi</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      {/* For small screen  */}
+      <div className="flex bg-cover bg-no-repeat bg-center bg-baige    shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] h-20  flex-row justify-between md:hidden">
+        <div className="flex absolute w-full z-50 p-5  justify-between items-center">
+          <Link href={"/"}>
+            <h3 className="font-bold">DB</h3>
+          </Link>
+          <div
+            onClick={() => setShowMenu(!showMenu)}
+            className={`w-10 h-10 border-2 border-white flex flex-col p-2 justify-around cursor-pointer`}
+          >
+            <div className="h-0.5 bg-black w-full"></div>
+            <div className="h-0.5 bg-black w-full"></div>
+            <div className="h-0.5 bg-black w-full"></div>
+          </div>
+        </div>
+
+        <div
+          className={`${
+            showMenu ? "h-screen" : "h-0"
+          } transition-all  duration-700 ease-in-out text-2xl bg-heading  fixed z-40 w-full bg-baige`}
+        >
+          <div
+            className={`${
+              showMenu ? "translate-y-60" : "-translate-y-60 opacity-0"
+            } flex justify-center flex-col gap-6 items-center text-center transition-all duration-700 ease-in-out`}
+          >
+            <h5>Home</h5>
+            <h5>Mama Chakra</h5>
+            <h5>Drone Buddy</h5>
+
+            <h5 onClick={() => window.open("tel:9322418319")}>Contact Us</h5>
+            {/* <Button className="bg-[#15D783]"  variant="outline"> Contact us!</Button> */}
+
+            <Select>
+              <SelectTrigger className="w-[180px] border-none outline-none ">
+                <SelectValue placeholder="Select a language" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup className=" border-none outline-none">
+                  <SelectLabel>Language</SelectLabel>
+                  <SelectItem value="en">English</SelectItem>
+                  <SelectItem value="hi">Hindi</SelectItem>
+                  <SelectItem value="mr">Marathi</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
